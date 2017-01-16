14f6b3ada177dace33f41d55c8e3c6b0168bfe02
Anthony Good <anthony@quipper.com>
2017-01-16 22:35:18 +0000

    more tests for boggleReducer

--- src/reducers/__tests__/boggle-reducer.test.js
+++ src/reducers/__tests__/boggle-reducer.test.js
@@ -4,21 +4,47 @@ describe("boggleReducer", () => {
   const state = boggle()
 
   it("returns default state when state parameter is undefined", () => {
+    expect(state.gamePhase).toBe("notStarted")
     expect(state.currentWord).toEqual([])
     expect(state.foundWords).toEqual([])
-    expect(state.grid).toBeDefined()
+    expect(state.grid).toEqual([])
     expect(state.score).toEqual(0)
     expect(state.selecting).toBe(false)
   })
 
-  it("updates state for the action START_SELECTING_LETTERS", () => {
-    const newState = boggle(state, {type: "START_SELECTING_LETTERS"})
-    expect(newState.selecting).toBe(true)
+  describe("START_SELECTING_LETTERS", () => {
+    it("updates state for the action START_SELECTING_LETTERS", () => {
+      const newState = boggle(state, {type: "START_SELECTING_LETTERS"})
+      expect(newState.selecting).toBe(true)
+    })
   })
 
-  it("updates state for the action END_SELECTING_LETTERS", () => {
-    const newState = boggle(state, {type: "END_SELECTING_LETTERS"})
-    expect(newState.selecting).toBe(false)
+  describe("END_SELECTING_LETTERS", () => {
+    it("updates state for the action END_SELECTING_LETTERS", () => {
+      const newState = boggle(state, {type: "END_SELECTING_LETTERS"})
+      expect(newState.selecting).toBe(false)
+    })
+  })
+
+  describe("START_GAME", () => {
+    const grid = ["someGrid"]
+    const newState = boggle(state, { type: "START_GAME", grid })
+
+    it("adds grid to state", () => {
+      expect(newState.grid).toEqual(grid)
+    })
+
+    it("sets gamePhase to 'started'", () => {
+      expect(newState.gamePhase).toBe("started")
+    })
+  })
+
+  describe("END_GAME", () => {
+    const newState = boggle(state, { type: "END_GAME" })
+
+    it("sets gamePhase to 'finished'", () => {
+      expect(newState.gamePhase).toBe("finished")
+    })
   })
 
   describe("ADD_LETTER", () => {
@@ -38,15 +64,17 @@ describe("boggleReducer", () => {
       }
     )
 
-    expect(newState.currentWord).toEqual([
-      {
-        letter: "X",
-        baseValue: 1,
-        multiplier: 5,
-        x: 1,
-        y: 2,
-        value: 5
-      }
-    ])
+    it("adds letter to currentWord", () => {
+      expect(newState.currentWord).toEqual([
+        {
+          letter: "X",
+          baseValue: 1,
+          multiplier: 5,
+          x: 1,
+          y: 2,
+          value: 5
+        }
+      ])
+    })
   })
 })
--- src/reducers/boggle-reducer.js
+++ src/reducers/boggle-reducer.js
@@ -4,10 +4,19 @@ import Checker from "../lib/Checker"
 
 const DEFAULT_GRID_SIZE = 4
 
+// const DEFAULT_STATE = {
+//   currentWord: [],
+//   foundWords: [],
+//   grid: makeGrid(DEFAULT_GRID_SIZE),
+//   score: 0,
+//   selecting: false
+// }
+
 const DEFAULT_STATE = {
+  gamePhase: "notStarted",
   currentWord: [],
   foundWords: [],
-  grid: makeGrid(DEFAULT_GRID_SIZE),
+  grid: [],
   score: 0,
   selecting: false
 }
@@ -18,43 +27,42 @@ const submitWord = (state, word) => {
     (letter) => { return letter.letter }
   ).join("").toLowerCase()
 
-  console.log(currentWord)
-
   const isCorrect = Checker.check(currentWord)
-
-  console.log(isCorrect)
 
   if(isCorrect) {
     // Calculate word's value
-    const wordValue = state.currentWord.reduce((a, b) => { return a.value + b.value }, 0)
-
-    console.log(wordValue)
-
-    const newScore  = state.score + wordValue
-
-    console.log(newScore)
+    const wordValue = state.currentWord.reduce((a, b) => { return a + b.value }, 0)
+    const score  = state.score + wordValue
 
     // Add word to foundWords
     const foundWords = state.foundWords + state.currentWord
 
     // Compose new state
-    return Object.assign({}, state, { foundWords, newScore, wordValue, currentWord: [], selecting: false })
+    return Object.assign({}, state, { foundWords, score, wordValue, currentWord: [], selecting: false })
   } else {
     // Just reset currentWord
     return Object.assign({}, state, { currentWord: [], selecting: false })
   }
-
 }
 
 export default (state = DEFAULT_STATE, action = {}) => {
   switch (action.type) {
+    case TYPES.START_GAME:
+      return Object.assign({}, state, { gamePhase: "started", grid: action.grid })
+
+    case TYPES.END_GAME:
+      return Object.assign({}, state, { gamePhase: "finished" })
+
     case TYPES.START_SELECTING_LETTERS:
       return Object.assign({}, state, { selecting: true })
+
     case TYPES.END_SELECTING_LETTERS:
       return submitWord(state)
+
     case TYPES.ADD_LETTER:
       const newWord = [ ...state.currentWord, Object.assign({}, action.letter) ]
       return Object.assign({}, state, {currentWord: newWord})
+
     default:
       return state
   }
